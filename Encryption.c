b7567e498e2eefb5eff7e3898b78a431eca184eb
benlaflen <114254189+benlaflen@users.noreply.github.com>
2023-05-20 14:36:07 -0700

    awrgsfv

--- Encryption.c
+++ Encryption.c
@@ -10,51 +10,41 @@ int *hash(char *key) {
     }
 }
 
-void encryptFile(const char password, const char fileName) {
+void encryptFile(char *password, char *fileName) {
     FILE *file = fopen(fileName, "rb");
     if (file == NULL) {
         printf("Unable to open file: %s\n", fileName);
         return;
     }
-
-    // Get the file name (without extension) as the seed for the random number generator
-    char seed = strdup(password);
-    char dot = strrchr(seed, '.');
-    if (dot != NULL) {
-        dot = '\0';
-    }
-
     // Seed the random number generator
-    srand(hash(seed));
+    srand(*hash(password));
     fclose(file);
-    file = fopen(fileName, "wb");
     // Create an output file with ".encrypted" extension
-    /* char outputFileName[256]; */
-    /* snprintf(outputFileName, sizeof(outputFileName), "%s.encrypted", fileName); */
-    /* FILEoutputFile = fopen(outputFileName, "wb"); */
-    /* if (outputFile == NULL) { */
-    /*     printf("Unable to create output file: %s\n", outputFileName); */
-    /*     fclose(file); */
-    /*     return; */
-    /* } */
+    char outputFileName[256]; 
+    snprintf(outputFileName, sizeof(outputFileName), "%s.encrypted", fileName); 
+    FILE *outputFile = fopen(outputFileName, "wb"); 
+    if (outputFile == NULL) { 
+        printf("Unable to create output file: %s\n", outputFileName); 
+        fclose(file); 
+        return; 
+    } 
 
     // Read and encrypt the content byte by byte
     int byte;
-    while ((byte = fgetc(fileName)) != EOF) {
+    while ((byte = fgetc(file)) != EOF) {
         // Generate a random key between 1 and 255
         int key = rand() % 255 + 1;
 
         // XOR the byte with the key to encrypt
         byte = byte ^ key;
-
         // Write the encrypted byte to the output file
-        fputc(byte, file);
+        fputc(byte, outputFile);
     }
 
     printf("File encrypted successfully. Output file: %s\n", fileName);
-
     fclose(file);
-    free(seed);
+    fclose(outputFile);
+    remove(fileName);
 }
 
 /*int main() {
