c30d261b37baa326a29096ed5c44ca9acdadb673
benlaflen <114254189+benlaflen@users.noreply.github.com>
2023-05-20 13:25:01 -0700

    Update GUI.c
    Added a bunch

--- GUI.c
+++ GUI.c
@@ -1,1 +1,19 @@
 #include <stdlib.h>
+#include "Encryption.h"
+#include "Decryption.h"
+
+int main(int argc, char **argv) {
+    if(argc != 3) {
+        printf("Incorrect number of arguments\n\nCorrect Usage:\nEncryption: ./Encrypter encrypt [Filename] [Password]\nDecryption: ./Encrypter decrypt [Filename] [Password]");
+    }
+    if(argv[1] == "encrypt") {
+        char *filename = argv[2];
+        char *password = argv[3];
+        char result;
+        printf("Verify the following is correct:\n\n")
+    } else if(argv[1] == "decrypt") {
+
+    } else {
+        printf("Invalid use case\n\nCorrect Usage:\nEncryption: ./Encrypter encrypt [Filename] [Password]\nDecryption: ./Encrypter decrypt [Filename] [Password]");
+    }
+}
